f0d2b962bfca1e240650808010ef1d0152065452
Justin Mills <me@jtsmills.com>
2025-04-28 09:59:09 -0400

    Package Bump Apr 28

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule EventStore.Mixfile do
     [
       app: :eventstore,
       version: @version,
-      elixir: "~> 1.17.2",
+      elixir: "~> 1.18.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
       description: description(),
